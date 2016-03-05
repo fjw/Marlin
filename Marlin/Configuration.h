a79c54fa23609702515ae66015685d3c4c44c492
fjw <fjw@garx.de>
2016-03-05 17:17:28 +0100

    feedratentest

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -595,8 +595,8 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 
 
 //todo
-#define DEFAULT_MAX_FEEDRATE          {65000, 65000, 65000, 25}    // (mm/sec)
-#define DEFAULT_MAX_ACCELERATION      {100,100,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
+#define DEFAULT_MAX_FEEDRATE          {11000, 11000, 65000, 25}    // (mm/sec)
+#define DEFAULT_MAX_ACCELERATION      {600,600,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 //todo
 
 
